194604396ddfecb212591c14e7270a43c7b75b89
Swarangii <swarangiyadav557@gmail.com>
2024-09-17 22:47:43 +0530

    4. AI Chat Bot by API of Gemini

--- Chat-Bot.py
+++ Chat-Bot.py
@@ -1,7 +1,5 @@
 import google.generativeai as genai
-
 genai.configure(api_key="AIzaSyCnCe_GlNrV-mf7zPECGdGsVNk5JhBmrz8")
-
 # for i in genai.list_models():
 #     print(i.name)
 def read(message):
@@ -10,12 +8,9 @@ def read(message):
      print(chunk.text)
 
 model=genai.GenerativeModel("gemini-pro")
-
 history=[]
 chat=model.start_chat(history=history)
-
 chat.send_message("your name is Dexy.")
-
 def chatting(text):
     res=chat.send_message(text)
     read(res)
